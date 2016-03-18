89b52108a882ec55d84bf6f9b53838127300e703
minamorl <minamorl@users.noreply.github.com>
2016-03-19 00:57:01 +0900

    initial commit
    Karma, Webpack and Babel is ready.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './app/entry.js',
+  output: {
+    path: __dirname + "/dist",
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+  },
+  module: {
+    loaders: [
+      { 
+        test: /\.jsx?$/, 
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: "#inline-source-map"
+}
