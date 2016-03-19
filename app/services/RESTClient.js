469283bb0e7efd3d8af8fd051eac7bb9c8822e41
minamorl <minamorl@users.noreply.github.com>
2016-03-19 22:50:33 +0900

    Add RESTClient service and test

---
+++ app/services/RESTClient.js
@@ -0,0 +1,37 @@
+import request from 'superagent'
+import { Enum } from 'enumify'
+
+export class MethodType extends Enum {}
+MethodType.initEnum(['GET', 'PUT', 'POST', 'DELETE'])
+
+export class RESTClient {
+  constructor() {
+    this.endpoints = {}
+  }
+  request(url, method, query={}, params={}) {
+    // Type Checking 
+    if (!(typeof url === 'string' || url instanceof String)) {
+      throw TypeError("url must be instance of String")
+    }
+    if (!(method instanceof MethodType)) {
+      throw TypeError("method must be instance of methodType")
+    }
+    return new Promise((resolve, reject) => {
+      request(method.name, url)
+        .query(query)
+        .send(params)
+        .end((err, res) => {
+          if (err !== null) reject(err)
+          resolve(res)
+        })
+    })
+  }
+  defineEndpoint(name, url) {
+    if(name === undefined || url === undefined)
+      throw TypeError("defineEndpoint must be given 2 arguments: url, name")
+    
+    this.endpoints[name] = (method) => {
+      return this.request(url, method)
+    }
+  }
+}
