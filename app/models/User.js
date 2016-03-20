eddd62ae2b13961c9654c4af1da8a9b10b93e623
minamorl <minamorl@users.noreply.github.com>
2016-03-21 00:45:43 +0900

    Implement User model
    Add login/logout methods and username property.

--- app/models/User.js
+++ app/models/User.js
@@ -1,3 +1,5 @@
+import Cookies from "js-cookie"
+
 export default class User {
   constructor() {
     this._isLoggedIn = false
@@ -11,10 +13,14 @@ export default class User {
     // create a *dummy* promise object. 
     // this must be replaced by *real* API calling.
     return new Promise((resolve, reject) => {
-      if(result) resolve()
+      if(true) resolve()
       reject()
     }).then(() => {
-      // create cookies.
+      // set cookies.
+      Cookies.set("auth", "authed")
+      Cookies.set("username", username)
+      // set status
+      this._isLoggedIn = true
     }).catch(() => {
       // do nothing. just prevend from error.
     })
@@ -22,8 +28,15 @@ export default class User {
   logout() {
     this._isLoggedIn = false
     // destroy cookies.
+    Cookies.remove("auth")
   }
   get isLoggedIn() {
     return this._isLoggedIn
+  }
+  get username() {
+    if (this.isLoggedIn)
+      return Cookies.get("username")
+    else
+      throw Error("User is not logged in.")
   }
 }
--- tests/models/UserSpec.js
+++ tests/models/UserSpec.js
@@ -1,5 +1,7 @@
 import User from "../../app/models/User.js"
 
+import Cookies from "js-cookie"
+
 describe("User", () => {
   let user
   beforeEach(() => {
@@ -13,6 +15,19 @@ describe("User", () => {
   })
   it("has isLoggedIn property (getter only)", () => {
     expect(user.isLoggedIn).toBeDefined()
+  })
+  it("has username property for a loginned user", done => {
+    const correctCredential = {
+      username: "username",
+      password: "password"
+    }
+    user.login(correctCredential).then(() => {
+      expect(user.username).toBe(correctCredential.username)
+      done()
+    })
+  })
+  it("raises an error when username is accessed without login process", () => {
+    expect(() => { user.username }).toThrowError(Error)
   })
   describe("login method", () => {
     it("accepts when passed correct values", () => {
@@ -37,6 +52,17 @@ describe("User", () => {
       }
       const promise = user.login(correctCredential)
       expect(promise instanceof Promise).toBe(true)
+    })
+  })
+  describe("logout method", () => {
+    it("destroy auth cookie", () => {
+      Cookies.set("auth", "authed")
+      user.logout()
+      expect(Cookies.get("auth")).not.toBeDefined()
+    })
+    it("always returns nothing", () => {
+      const result = user.logout()
+      expect(result).not.toBeDefined()
     })
   })
 })
